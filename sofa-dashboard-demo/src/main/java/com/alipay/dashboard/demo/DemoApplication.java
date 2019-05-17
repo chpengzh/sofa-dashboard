d6837c412e756d3493e7cf412a7f85ed44681988
chpengzh <chpengzh@foxmail.com>
2019-05-18 07:42:28 +0800

    提交测试demo应用

---
+++ sofa-dashboard-demo/src/main/java/com/alipay/dashboard/demo/DemoApplication.java
@@ -0,0 +1,18 @@
+package com.alipay.dashboard.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ *
+ *
+ * @author chpengzh.
+ * @since 2019-05-17
+ */
+@SpringBootApplication
+public class DemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DemoApplication.class, args);
+    }
+}
